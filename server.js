abc6a036afa06490386a29a9f817fe3c0b642b53
Andreas Siebert (drdrej) <ask@touchableheroes.com>
2016-05-09 17:01:39 +0200

    add forward()

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var restify = require('restify');
 var jsonpointer = require( 'jsonpointer.js' );
+var unirest = require( "unirest" );
+
 // var querystring = require('querystring');
 
 var ResponseHandler = function(server, requestConfig) {
@@ -13,17 +15,67 @@ ResponseHandler.prototype.match = function () {
 
 };
 
-ResponseHandler.prototype.response = function( resultFnc ) {
+ResponseHandler.prototype.forward = function( fwdUrl, fwdResultHandlerFnc ) {
+    _checkMethod();
+
+    this.server[this.requestConfig.method] (
+        this.requestConfig.endpoint,
+        function ( inReq, inRes, inNext ) {
+            // log(inReq);
+
+            var fwdReq = unirest.get( fwdUrl );
+
+            fwdReq.method( this.requestConfig.method );
+
+            if( inReq.query ) {
+                console.log( ">> copy query-params ... ");
+                fwdReq.query( inReq.query );
+            }
+
+            if( inReq.params ) {
+                console.log( ">> copy form-params ... ");
+                fwdReq.form( inReq.params );
+            }
+
+            fwdReq.headers({
+                'Content-Type': 'text/html'
+            });
+
+            fwdReq.end(
+                function success(response) {
+                    console.log(response);
+
+                    inRes.send(response.body); // BODY wird einfach weiter kopiert.
+
+                    inNext();
+                },
+
+                function error( err) {
+                    console.log("err: " + err);
+                });
+        }
+    );
+
+};
+
+// config check methoden :::
+// -----------------------------------------------------
+function _checkMethod() {
     if( !this.requestConfig.method ) {
         throw "MISSING param in server.config";
     }
 
-    function log( req ) {
-       console.log( "> body: ");
-       console.log( req.body );
-       console.log( "< body" );
-    }
+    return true;
+};
 
+ResponseHandler.prototype.response = function( resultFnc ) {
+    _checkMethod();
+
+    function log( req ) {
+        console.log( "> body: ");
+        console.log( req.body );
+        console.log( "< body" );
+    }
 
     this.server[this.requestConfig.method] (
         this.requestConfig.endpoint,
@@ -77,13 +129,13 @@ var Server = function( config ) {
     server.use(restify.fullResponse());
 
     /*
-    server.on( 'after', restify.auditLogger({
-        log: bunyan.createLogger({
-            name: 'audit',
-            stream: process.stdout
-        })
-    }));
-    */
+     server.on( 'after', restify.auditLogger({
+     log: bunyan.createLogger({
+     name: 'audit',
+     stream: process.stdout
+     })
+     }));
+     */
 
     console.log("> server initialization completed." );
     this.server = server;
@@ -101,18 +153,11 @@ Server.prototype.get = function( path ) {
     return jsonpointer.get(this.appState, path );
 };
 
-/*
-Server.prototype.push = function( path, value ) {
-
-};
-*/
 
 Server.prototype.start = function ( ) {
     this.server.listen( 8383, function () {
         console.log("rest-api successful started. let's play" );
-//        console.log('%s listening at %s', this.server.name, this.server.url);
     });
 };
 
 module.exports = Server;
-
