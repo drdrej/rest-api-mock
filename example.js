8b850b2ded4f9091d0746cfe0d1de326a8ad0955
drdrej <ask@touchableheroes.com>
2016-05-18 23:53:18 +0200

    add response()

--- example.js
+++ example.js
@@ -40,6 +40,48 @@ server.on({
          });
 
 
+
+
+server.on({
+    method: "get",
+    endpoint: "/item/x1"
+}).error( 403,
+    function () {
+        return { error: "Error message!" };
+    });
+
+
+server.on({
+    method: "get",
+
+    name: "xyz",
+    description: "dasdadasda",
+    pathPattern: "/item/:id",
+
+    endpoint: "/item/5",
+
+    testCase: 1, /* or name? */
+
+    params: [
+        "json:///json/path",
+        "query://param1",
+        "form://name1"
+    ],
+
+    match: function ( jsonPathVal, qP1, fName1 ) {
+        return true;
+    },
+
+    log: true
+
+}).response(
+    function ( jsonPathVal, qP1, fName1 ) {
+        return { success: ("Test" + jsonPathVal + ":" + qP1 + ":" + fName1) };
+    });
+
+
+
+
 server.on({
     method: "get",
     endpoint: "/items"
